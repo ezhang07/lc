299176623cb390c1c2103306e617d02e31b16e0c
Ethan Zhang <111698717+ezhang07@users.noreply.github.com>
2026-09-25 03:14:00 -0700

    Update: top-k-elements-in-list - submission-2

--- Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -1,29 +1,34 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
+        count frequencies using a hashmap
+
+        after that we use bucket sort, where index of array => frequency
+
+        nearest to the end has highest freq, based off of k, we just get the top k. 
         """
 
-        freqMap = {} # value : frequency
+        freq_map = {}
 
-        # build freqMap
-        for i, n in enumerate(nums):
-            freqMap[n] = freqMap.get(n, 0) + 1
+        for n in nums: # count freqs using a hashmap
+            freq_map[n] = 1 + freq_map.get(n, 0)
+
+        bucket_sort = [[] for i in range(len(nums) + 1)]
+
+        for n, freq in freq_map.items():
+            bucket_sort[freq].append(n)
         
-        bucketSort = [[] for _ in range(len(nums) + 1)]
-
-        for val, freq in freqMap.items():
-            bucketSort[freq].append(val) # consider multiple vals with same freq
-        
-
         res = []
-        for i in range(len(nums), -1, -1):
-            if k == 0:
-                return res
-            for n in bucketSort[i]:
-                res.append(n)
-                k-=1
+        for i in range(len(bucket_sort) - 1, -1, -1):
+            for val in bucket_sort[i]:
+                res.append(val)
+                if len(res) == k:
+                    return res
+        
+        
+        
 
-        return res
+
+        
+
+        
